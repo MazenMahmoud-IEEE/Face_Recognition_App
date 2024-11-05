b506f2340897eb2f5b57a6a028731ce8ef6f939e
Mazen Mahmoud <mazenmahmoud675@gmail.com>
2024-11-05 17:21:58 +0200

    Update face_app.py

--- face_app.py
+++ face_app.py
@@ -17,7 +17,7 @@ face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
 def upload_to_drive(file_path, file_name):
     SCOPES = ['https://www.googleapis.com/auth/drive.file']
     creds = service_account.Credentials.from_service_account_file(
-        r".\face-recognition-app-440518-e50c6b2c8d3a.json", scopes=SCOPES)
+        "face-recognition-app-440518-e50c6b2c8d3a.json", scopes=SCOPES)
     drive_service = build('drive', 'v3', credentials=creds)
 
     file_metadata = {'name': file_name, 'parents': ['17kt0LrNDI5IZpvwMML-S6r_kjGy5jLgR']}
